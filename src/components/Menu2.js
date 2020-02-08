055a30d966d6d3971034d66ff6a7f47b9c2aade0
ira <itrusevich@gmail.com>
2020-02-08 14:03:37 -0800

    progress

--- src/components/Menu2.js
+++ src/components/Menu2.js
@@ -2,14 +2,14 @@ import React from 'react';
 import MenuItem from "./MenuItem";
 
 
-function Menu1(props) {
+function Menu2(props) {
   return (
     <nav>
       <ul className='nav justify-content-left'>
-        {props.map(el => <MenuItem key={el.text} menuItem={el}/>)}
+        {props.menuItems2.map(el => <MenuItem key={el.text} menuItem={el}/>)}
       </ul>
     </nav>
   );
 }
 
-export default Menu1;
+export default Menu2;
