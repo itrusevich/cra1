b3dae3ebf753238e693e2a91c5498e3eb82c0162
ira <itrusevich@gmail.com>
2020-02-08 02:59:42 -0800

    Updates

---
+++ precache-manifest.f51826b50cac6285914ffaef52f76711.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1ce107e034e4ef2f65a26adeb03616",
+    "url": "/cra1/index.html"
+  },
+  {
+    "revision": "0155258e1772f3ae2527",
+    "url": "/cra1/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ea852d585273e6fa150e",
+    "url": "/cra1/static/css/main.d82a4a0b.chunk.css"
+  },
+  {
+    "revision": "0155258e1772f3ae2527",
+    "url": "/cra1/static/js/2.94d87881.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra1/static/js/2.94d87881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea852d585273e6fa150e",
+    "url": "/cra1/static/js/main.420cca99.chunk.js"
+  },
+  {
+    "revision": "358fadb87d4716f3bbce",
+    "url": "/cra1/static/js/runtime-main.04104b05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra1/static/media/logo.5d5d9eef.svg"
+  }
+]);
