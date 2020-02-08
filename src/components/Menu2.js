9e7fef88b9693e13878a22fb612911cf06c5f688
ira <itrusevich@gmail.com>
2020-02-08 13:54:29 -0800

    upd

---
+++ src/components/Menu2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MenuItem from "./MenuItem";
+
+
+function Menu1(props) {
+  return (
+    <nav>
+      <ul className='nav justify-content-left'>
+        {props.map(el => <MenuItem key={el.text} menuItem={el}/>)}
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu1;
